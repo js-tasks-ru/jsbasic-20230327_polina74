dcca180dd098152e34da507cf3c18d3398cee152
close2polly <poly2simon@gmail.com>
2023-04-10 20:00:56 +0400

    Решение четвертой задачи

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(users = [], age) {
+  let result = users
+    .filter(function (item) {
+      if (item.age <= age) {
+        return true;
+      }
+      return false;
+    })
+    .map(function (item) {
+      return item.name + ", " + item.balance;
+    })
+    .join("\n");
+  return result;
 }
