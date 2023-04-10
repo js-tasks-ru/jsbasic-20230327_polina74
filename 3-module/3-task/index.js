7590f5e1bf571093b31ddeb8ae1c121d810a67ff
close2polly <poly2simon@gmail.com>
2023-04-10 19:33:13 +0400

    Решение третьей задачи

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let newStr = str
+    .split("-")
+    .map((item, index) => {
+      if (index === 0) {
+        return item;
+      }
+      return item.at(0).toUpperCase() + item.slice(1);
+    })
+    .join("");
+
+  return newStr;
 }
