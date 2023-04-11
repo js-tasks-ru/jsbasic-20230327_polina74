efbdb554b69ec45f401ebe353afb5db999edce5c
close2polly <poly2simon@gmail.com>
2023-04-11 18:30:17 +0400

    Решение первой задачи

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement("ul");
+  for (const friend of friends) {
+    const elem = document.createElement("li");
+    elem.innerHTML = friend.firstName + " " + friend.lastName;
+    list.append(elem);
+  }
+  return list;
 }
