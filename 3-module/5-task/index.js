80f0a250f68dbe860d37f902fff31e56eca7359d
close2polly <poly2simon@gmail.com>
2023-04-11 18:00:25 +0400

    Решение пятой задачи

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,24 @@
 function getMinMax(str) {
-  // ваш код...
+  let newArr = str
+    .split(" ")
+    .filter(function (item) {
+      const value = parseFloat(item);
+      if (!isNaN(value)) {
+        return true;
+      }
+      return false;
+    })
+    .map(function (item) {
+      return parseFloat(item);
+    });
+
+  let max = Math.max(...newArr);
+  let min = Math.min(...newArr);
+
+  return {
+    min,
+    max,
+  };
 }
+
+
