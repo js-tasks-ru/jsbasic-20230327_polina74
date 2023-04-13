53989acaa157a9c39659b91bc5b15129a081dc6c
close2polly <poly2simon@gmail.com>
2023-04-13 14:49:29 +0400

    Решение третьей задачи

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  for (const row of table.lastElementChild.rows) {
+    let statusTd = row.lastElementChild;
+    let isAvailableTd = statusTd.dataset.available;
+    let sex = statusTd.previousElementSibling.innerHTML;
+    let age = row.firstElementChild.nextElementSibling.innerHTML;
+
+    if (isAvailableTd === "true") {
+      row.classList.add("available");
+    } else if (isAvailableTd === "false") {
+      row.classList.add("unavailable");
+    } else {
+      row.hidden = true;
+    }
+
+    if (sex === "m") {
+      row.classList.add("male");
+    } else if (sex === "f") {
+      row.classList.add("female");
+    }
+
+    if (Number(age) < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
